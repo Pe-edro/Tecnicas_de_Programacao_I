9e244e3d68b5e0b829b2872776cc9a69e650da29
PEDRO SENHOR MONTE <pedrosenhor67@gmail.com>
2026-08-08 20:14:52 +0000

    comiitado

---
+++ Java/Atv2.java
@@ -0,0 +1,10 @@
+public class Atv2{
+    String nome = "Pedro Senhor Monte";
+    int idade = 21;
+    float altura = 1.78;
+    float nota = 7.8;
+    String matricula = "confirmada";
+    pubic static void main(String[] args){
+        System.out.println("status:\n" + "idade: " + idade\n + "altura: " + altura\n + "nota: " = nota\n + "matricula: " + matricula);
+    }
+}
--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,11 @@
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Olá Mundo!");
-        System.out.println("Aula introdutoria a Java!");
-        System.out.println("Olá Mundo!");
+        System.out.println("Olá Mundo!");                   //print: mostra uma mensagem no terminal sem a quebra de linha 
+        System.out.println("Pedro Senhor Monte");           //println: mostra a mensagem no terminal com uma quebra de linha automatica
+        System.out.print("Curso: ");
+        System.out.println("Desenvolvimento de software multiplataforma");
+        System.out.print("Turma: ");
+        System.out.println("Técnicas de programação I");
     }
 }
