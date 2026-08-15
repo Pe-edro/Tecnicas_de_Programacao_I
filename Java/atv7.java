089814251a49ac8d4d05d593c9cd750a88b5e66f
PEDRO SENHOR MONTE <pedrosenhor67@gmail.com>
2026-08-15 20:02:17 +0000

    novos exercicios em atv7 matematica sem entrada de dados

--- Java/atv7.java
+++ Java/atv7.java
@@ -1,4 +1,5 @@
 public class atv7{
+    //exercicio 9
     public static void main(String[] args){
         int soma = 10 + 2;
         int menos = 10 -2;
@@ -10,5 +11,31 @@ public class atv7{
         System.out.println(vezes);
         System.out.println(dividi);
         System.out.println(modulo);
+   
+    //exercicio 10
+
+        System.out.println("atividade 10!");
+        int nota1 = 6;
+        int nota2 = 9;
+        int nota3 = 12;
+        float sua_media = (nota1 + nota2 + nota3) / 2;
+        System.out.println(sua_media);
+
+
+    //exercicio 11 
+        System.out.println("atividade 11!");
+        int base = 4;
+        int altura = 8;
+        int area = (base * altura);
+        System.out.println("A area da seu retangulo é: " + area);
+
+    //exercicio 12
+        System.out.println("Atividade 12!");
+        double celcius = 17.00;
+        double fahrenheit = celcius * 9 / 5 + 32;
+        System.out.println("conversao de " + celcius + " graus para fahrenheit é: " + fahrenheit);
+
+    //exercicio 13
+        System.out.println("Atividade 13!");
     }
 }
