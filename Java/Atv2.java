b5da2e8ae920fd65645c797ca2f6be1a8a13ea68
PEDRO SENHOR MONTE <pedrosenhor67@gmail.com>
2026-08-15 19:02:31 +0000

    commit

--- Java/Atv2.java
+++ Java/Atv2.java
@@ -1,10 +1,12 @@
 public class Atv2{
+    public static void main(String[] args){
     String nome = "Pedro Senhor Monte";
     int idade = 21;
-    float altura = 1.78;
-    float nota = 7.8;
+    float altura = 1.78f;
+    float nota = 7.8f;
     String matricula = "confirmada";
-    pubic static void main(String[] args){
-        System.out.println("status:\n" + "idade: " + idade\n + "altura: " + altura\n + "nota: " = nota\n + "matricula: " + matricula);
+    
+        System.out.println("status:\n" + "idade: " + idade + "\naltura: " + altura + "\nnota: " + nota + "\nmatricula: " + matricula);
     }
+
 }
---
+++ Java/atv3.java
@@ -0,0 +1,7 @@
+public class atv3{
+    public static void main(String[] args){
+        System.out.println("Nome: Pedro Senhor Monte");
+        System.out.println("curso: Desenvolvimento de Software multiplataforma");
+        System.out.println("Turma: Tecnicas de Programação I");
+    }
+}
